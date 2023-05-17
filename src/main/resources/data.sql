425f8bbe7fa81ca4400e0bc6bc8720dca81d80f8
Y0ucandev <112213953+Y0ucandev@users.noreply.github.com>
2023-05-17 17:10:12 +0200

    Revert "Feature/coderef"

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
- insert
-    into
-        users
-        (city, street, email, firstname, lastname, password, role)
-    values
-        ('Bydgoszcz','Zielona','admin@wp.pl','Michał','Ważny','admin','ADMIN');
